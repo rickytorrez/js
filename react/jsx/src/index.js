fc43b09d50a50fe874e4470604db7c6cc2127d04
rickytorrez <torrez.ricky@ymail.com>
2019-08-13 22:33:06 -0400

    react intro

--- react/jsx/src/index.js
+++ react/jsx/src/index.js
@@ -4,7 +4,22 @@ import ReactDom from 'react-dom';
 
 // create a react component
 const App = () => {
-    return <div>Hi There!</div>;
+
+    const buttonText = { text: 'Click Me' };
+    const style = { backgroundColor: 'blue', color: 'white' };
+    const labelText = 'Enter name:';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                { labelText }
+            </label>
+            <input id="name" type="text"/>
+            <button style={ style }>
+                { buttonText.text }
+            </button>
+        </div>
+    );
 };
 
 // take the react component and show it on the screen, link it to the id of root in the public/index.html file
