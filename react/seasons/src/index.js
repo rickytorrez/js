80bebcb9969ac8b9f6dddb71b0daa9bd3f2dca5f
rickytorrez <torrez.ricky@ymail.com>
2019-08-20 21:20:04 -0400

    adding state

--- react/seasons/src/index.js
+++ react/seasons/src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render(){
+
+    // super is a reference to the parent's constructor function
+    constructor(props){
+        super(props);
+
+        // this is the only time we do direct assignment to this.state
+        this.state = { lat: null };
+
         // geolocation to get user's location
             // position is the response observable
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                // to update our state object, we call setState
+                this.setState({ lat: position.coords.latitude })
+            },
             error => console.log(error) 
         );
+    }
 
-        return <div>Latitude: </div>
+    // React says we have to define render
+    render(){
+        return <div>Latitude: { this.state.lat } </div>
     }
 }
 
