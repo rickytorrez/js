cd083d3128c2732e7075ea891292881b819abebb
rickytorrez <torrez.ricky@ymail.com>
2019-08-20 21:46:29 -0400

    conditional rendering

--- react/seasons/src/index.js
+++ react/seasons/src/index.js
@@ -8,22 +8,32 @@ class App extends React.Component {
         super(props);
 
         // this is the only time we do direct assignment to this.state
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         // geolocation to get user's location
             // position is the response observable
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // to update our state object, we call setState
-                this.setState({ lat: position.coords.latitude })
+                this.setState({ lat: position.coords.latitude });
             },
-            error => console.log(error) 
+            error => {
+                this.setState({ errorMessage: error.message });
+            }
         );
     }
 
     // React says we have to define render
     render(){
-        return <div>Latitude: { this.state.lat } </div>
+        if( this.state.errorMessage && !this.state.lat ){
+            return <div>Error: { this.state.errorMessage }</div>
+        }
+
+        if( !this.state.errorMessage && this.state.lat ){
+            return <div>Latitude: { this.state.lat }</div>
+        }
+
+        return <div>Loading... </div>
     }
 }
 
