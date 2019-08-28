d55a9212737594a469da833b8c473473578dc98f
rickytorrez <torrez.ricky@ymail.com>
2019-08-28 00:04:47 -0400

    communicating b/w child and parent components

--- react/pictures/src/components/App.js
+++ react/pictures/src/components/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+
+    // defines callback function
+    onSearchSubmit(term){
+        console.log(term);
+    }
+
+    render(){
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={ this.onSearchSubmit }/>
+            </div>
+        );
+    }
+}
 
 export default App;
--- react/pictures/src/components/SearchBar.js
+++ react/pictures/src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = { term: '' };
+
+    // event.preventDefault() prevents submission of the form and refreshing of the page
+    // the arrow function will make sure that the value of 'this' is always equal to the value of the instance 
+    // of our SearchBar
+    onFormSubmit = event =>  {
+        event.preventDefault();
+        
+        this.props.onSubmit(this.state.term);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={ this.onFormSubmit }  className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />   
+                        <input 
+                            type="text" 
+                            value={ this.state.term } 
+                            onChange={ (e) => this.setState({ term: e.target.value }) }
+                        />   
                     </div>
                 </form>
             </div>
