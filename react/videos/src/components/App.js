94b7ce35b7bed59dce4bcd9c9e66a334c0e49018
rickytorrez <torrez.ricky@ymail.com>
2019-09-02 15:32:46 -0400

    billions and billions of callbacks

--- react/videos/src/components/App.js
+++ react/videos/src/components/App.js
@@ -6,7 +6,10 @@ import VideoList from './VideoList';
 
 class App extends React.Component {
 
-    state = { videos: [] };
+    state = { 
+        videos: [],
+        selectedVideo: null 
+    };
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -18,11 +21,18 @@ class App extends React.Component {
         this.setState({ videos: response.data.items })
     };
 
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
     render(){
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={ this.onTermSubmit }/>
-                <VideoList videos={ this.state.videos }/>
+                <VideoList 
+                    videos={ this.state.videos }
+                    onVideoSelect={ this.onVideoSelect }
+                />
             </div>
         )
     }
--- react/videos/src/components/VideoList.js
+++ react/videos/src/components/VideoList.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const VideoList = (props) => {
-    // props.videos
-    return<div>{ props.videos.length }</div>
+import VideoItem from './VideoItem';
+
+// videos is the same as props
+// onVideoSelect is a callback from App.js 
+const VideoList = ({ videos, onVideoSelect }) => {
+
+    // map over the array that we receive from props for every single video
+    const renderedList = videos.map((video) => {
+        return (
+            <VideoItem 
+                video={ video } 
+                onVideoSelect={ onVideoSelect }
+            />
+        )
+    })
+
+    return<div className="ui relaxed divided list">{ renderedList }</div>
 }
 
 export default VideoList;
