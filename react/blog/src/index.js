6ae3f031fdeb618d19ed37be07e0ccb79bd7349c
rickytorrez <torrez.ricky@ymail.com>
2019-09-11 21:14:13 -0400

    notes

--- react/blog/src/index.js
+++ react/blog/src/index.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+//thunk added as a middleware
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
