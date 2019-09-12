49112fd43eb47f28ff6b3b236af34bafb7be37c2
rickytorrez <torrez.ricky@ymail.com>
2019-09-12 17:38:26 -0400

    switch and default

--- react/blog/src/reducers/postReducer.js
+++ react/blog/src/reducers/postReducer.js
@@ -1,3 +1,9 @@
-export default () => {
-    return 123;
+// state is initialized as an empty array since it will return a list of blogs
+export default (state = [], action) => {
+    switch(action.type){
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
 };
