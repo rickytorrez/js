be96ef91968d053b8b48b42bab185de113a188a6
rickytorrez <torrez.ricky@ymail.com>
2019-09-13 11:09:28 -0400

    switch notes

--- react/blog/src/reducers/postReducer.js
+++ react/blog/src/reducers/postReducer.js
@@ -1,4 +1,5 @@
 // state is initialized as an empty array since it will return a list of blogs
+// switch case is used instead of if/else statements - default is our safe catch
 export default (state = [], action) => {
     switch(action.type){
         case 'FETCH_POSTS':
