4075c0314c9f39b4395343e01ddead4f0bbe975a
rickytorrez <torrez.ricky@ymail.com>
2019-09-17 18:49:36 -0400

    ation on action

--- react/blog/src/actions/index.js
+++ react/blog/src/actions/index.js
@@ -1,19 +1,34 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    // _.uniq returns an array with the unique userIds
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-// pass in the id of the user we want to fetch
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-}
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${ id }`);
     
     dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+}; 
+
+// MEMOIZED //
+
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${ id }`);
+    
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
--- react/blog/src/components/PostList.js
+++ react/blog/src/components/PostList.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import UserHeader from './UserHeader';
 
 class PostList extends Component {
 
     componentDidMount(){
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     // list rendering logic - JSX
@@ -37,4 +37,4 @@ const mapStateToProps = (state) => {
     return { posts: state.posts };
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
