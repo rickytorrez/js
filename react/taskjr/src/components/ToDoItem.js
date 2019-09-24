6049a259497dc85f34bea36ab2e42eff3eabd0a0
rickytorrez <torrez.ricky@ymail.com>
2019-09-24 19:12:21 -0400

    single task add

--- react/taskjr/src/components/ToDoItem.js
+++ react/taskjr/src/components/ToDoItem.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ToDoItem = () => {
-    return(
-        <div>Item</div>
-    )
+class ToDoItem extends Component{
+
+    state = { task: '' };
+
+    onTaskInput = (e) => {
+        e.preventDefault();
+        // add props
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={ this.onTaskInput }>
+                    <label>Task:</label>
+                    <input 
+                        type="text" 
+                        onChange={ (e) => this.setState({ task: e.target.value }) }></input>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default ToDoItem;
