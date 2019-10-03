51b66c5749ca2ed4ef35b56c9edd7d09b2f1ce50
rickytorrez <torrez.ricky@ymail.com>
2019-10-02 20:21:47 -0400

    scrollview, styling, binding

--- react-native/complete-guide/App.js
+++ react-native/complete-guide/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -39,9 +39,13 @@ export default function App() {
           value={ enteredGoal }/>
         <Button title="ADD" onPress={ addGoalHandler }/>
       </View>
-      <View>
-        { courseGoals.map( (goal) => <Text key={ goal }>{ goal }</Text> ) }
-      </View>
+      <ScrollView>
+        { courseGoals.map( (goal) => 
+          <View key={ goal } style={ styles.listItem }>
+            <Text> { goal }</Text>
+          </View>) 
+        } 
+      </ScrollView>
     </View>
   );
 }
@@ -60,5 +64,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: .5,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
