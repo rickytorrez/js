3d117fe95edccfc331be636d682c17b2bf156073
rickytorrez <torrez.ricky@ymail.com>
2019-11-04 13:08:42 -0500

    context and state

--- react-native/blog/src/screens/Index.js
+++ react-native/blog/src/screens/Index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { View, StyleSheet, Button, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, FlatList, Button } from 'react-native';
+
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+
+    const { data, addBlogPost } = useContext(BlogContext);
+
     return(
         <View>
             <Text>Index Screen Shown</Text>
+            <Button 
+                title='add post'
+                onPress={ addBlogPost }/>
+            <FlatList
+                keyExtractor={( blogPost ) => blogPost.title }
+                data={ data }
+                renderItem={({ item }) => {
+                    return <Text>{ item.title }</Text>
+                }}/>
         </View>
     )
 }
