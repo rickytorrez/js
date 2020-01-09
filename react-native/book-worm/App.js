412976734d117572ecb6ca7e59ceb51b6cf1dbdf
rickytorrez <torrez.ricky@ymail.com>
2020-01-08 21:14:46 -0500

    props to hide or show add button

--- react-native/book-worm/App.js
+++ react-native/book-worm/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView, 
+  TouchableOpacity,
+  TextInput 
+} from 'react-native';
 
 import BookCount from './components/BookCount';
+import { Ionicons } from '@expo/vector-icons';
 
 class App extends React.Component {
 
@@ -10,18 +18,95 @@ class App extends React.Component {
     this.state = {
       totalCount: 0,
       readingCount: 0,
-      readCount: 0
+      readCount: 0,
+      isAddNewBookVisible: false
     };
+  };
+
+  showAddNewBook = () => {
+    this.setState({ isAddNewBookVisible: true });
+  };
+
+  hideAddNewBook = () => {
+    this.setState({ isAddNewBookVisible: false });
   };
 
   render(){
     return (
       <View style={{ flex: 1 }}>
         <SafeAreaView />
+
         <View style={ styles.header }>
           <Text style={ styles.appTitle }>Book Worm</Text>
         </View>
-        <View style={{ flex: 1 }}></View>
+
+        <View style={{ flex: 1 }}>
+          { this.state.isAddNewBookVisible && (
+            <View style={{ height: 50, flexDirection:'row' }}>
+              <TextInput 
+                style={{ 
+                  flex: 1, 
+                  backgroundColor: '#ececec',
+                  paddingLeft: 5
+                }} 
+                placeholder='Enter Book Name' 
+                placeholderTextColor= 'grey'
+              />
+              <TouchableOpacity>
+                <View style={{ 
+                    width: 50,
+                    height: 50,
+                    backgroundColor: '#a5deba',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                  <Ionicons 
+                    name='ios-checkmark' 
+                    color='white' 
+                    size={ 40 }
+                  />
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={ this.hideAddNewBook }>
+                <View style={{ 
+                    width: 50,
+                    height: 50,
+                    backgroundColor: '#deada5',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                  <Ionicons 
+                    name='ios-close' 
+                    color='white' 
+                    size={ 40 }
+                  />
+                </View>
+              </TouchableOpacity>
+          </View>
+          )}
+
+          <TouchableOpacity 
+            onPress={ this.showAddNewBook }
+            style={{ 
+              position: 'absolute', 
+              bottom: 20, 
+              right: 20 }}>
+            <View style={{ 
+              width: 50, 
+              height: 50, 
+              borderRadius: 25, 
+              backgroundColor: '#AAD1E6', 
+              alignItems: 'center', 
+              justifyContent: 'center' }}>
+              <Text style={{ 
+                color: 'white', 
+                fontSize: 30 }}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        
+
         <View style={ styles.footer }>
           <BookCount 
             title='Total'
@@ -33,6 +118,7 @@ class App extends React.Component {
             title='Read'
             count={ this.state.readCount }/>        
         </View>
+
         <SafeAreaView />
       </View>
     );
