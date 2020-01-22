50e51fceeaa2ec26f1f254d110d4a50b9fd28856
rickytorrez <torrez.ricky@ymail.com>
2020-01-22 10:36:46 -0500

    font import

--- react-native/number/App.js
+++ react-native/number/App.js
@@ -1,16 +1,40 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// prolongs the screen until a certain task is done
+import { AppLoading } from 'expo';
+
+// load font import
+import * as Font from 'expo-font';
+
 import Header from './components/Header';
 import StartGameScreen from './screen/StartGameScreen';
 import GameScreen from './screen/GameScreen';
 import GameOverScreen from './screen/GameOverScreen';
 
+// load font function
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
 
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);  
-  console.log('guess rounds', guessRounds);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded){
+    return (
+      <AppLoading 
+        startAsync={ fetchFonts } 
+        onFinish={ () => setDataLoaded(true) } 
+        onError={ (err) => console.log(err) }
+      />
+    );
+  }
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
