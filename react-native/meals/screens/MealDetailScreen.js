441fd7de93d90d762a71f51eee8326ae95ebc409
rickytorrez <torrez.ricky@ymail.com>
2020-01-31 11:20:31 -0500

    header button, meal deet screen

--- react-native/meals/screens/MealDetailScreen.js
+++ react-native/meals/screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummydata';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = (props) => {
 
@@ -20,7 +22,19 @@ MealDetailScreen.navigationOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParam('mealItemId');
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title
+        headerTitle: selectedMeal.title,
+        headerRight: () => 
+            <HeaderButtons
+                HeaderButtonComponent={ HeaderButton }>
+                <Item 
+                    title='Favorite'
+                    iconName='ios-star'
+                    onPress={ () => {
+                        console.log('mark as fave') 
+                        }
+                    }
+                />
+            </HeaderButtons>
     };
 };
 
