bebe883f42697e4c47f44426b3363300d79d57ea
rickytorrez <torrez.ricky@ymail.com>
2020-02-03 11:22:40 -0500

    styling changes

--- react-native/meals/components/CategoryGridTile.js
+++ react-native/meals/components/CategoryGridTile.js
@@ -28,8 +28,12 @@ const styles = StyleSheet.create({
         flex: 1,
         margin: 15,
         height: 150,
-        borderRadius: 15,
-        //overflow: 'hidden'
+        borderRadius: 15,        
+        elevation: 3,
+        overflow: 
+            Platform.OS === 'android' && Platform.Version >= 21 
+                ? 'hidden' 
+                : 'visible'
     },
     container: {
         flex: 1,
@@ -38,7 +42,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-        elevation: 3,
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
