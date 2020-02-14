488567956ae8db833d6239deb4e0ab9d8e954d5f
rickytorrez <torrez.ricky@ymail.com>
2020-02-14 12:15:26 -0500

    filters, swtich, useState

--- react-native/meals/screens/FiltersScreen.js
+++ react-native/meals/screens/FiltersScreen.js
@@ -1,24 +1,56 @@
 import React, { useState } from 'react';
-import { View, Text, Switch, StyleSheet } from 'react-native';
+import { View, Text, Switch, StyleSheet, Platform } from 'react-native';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 
+import Colors from '../constants/Colors';
+
+const FilterSwitch = (props) => {
+    return(
+        <View style={ styles.filterContainter }>
+            <Text>{ props.label }</Text>
+            {/* Switch manually manages their state */}
+            <Switch 
+                trackColor={ {true: Colors.primaryColor} }
+                thumbColor={ Platform.OS === 'android' ? Colors.accentColor : '' }
+                value={ props.value }
+                onValueChange={ props.onChange }    
+            />
+        </View>
+    );
+};
+
 const FiltersScreen = (props) => {
 
     const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isDairyFree, setIsDairyFree] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
 
     return (
         <View style={ styles.screen }>
             <Text style={ styles.title }>Available Filters</Text>
-            <View style={ styles.filterContainter }>
-                <Text>Gluten-Free </Text>
-                {/* Switch manually manages their state */}
-                <Switch 
-                    value={ isGlutenFree }
-                    onValueChange={ (newValue) => {setIsGlutenFree(newValue)} }    
-                />
-            </View>
+            <FilterSwitch 
+                label='Gluten Free'
+                value={ isGlutenFree }
+                onChange={ (newValue) => {setIsGlutenFree(newValue)} }    
+            />
+            <FilterSwitch 
+                label='Vegan'
+                value={ isVegan }
+                onChange={ (newValue) => {setIsVegan(newValue)} }    
+            />
+            <FilterSwitch 
+                label='Dairy Free'
+                value={ isDairyFree }
+                onChange={ (newValue) => {setIsDairyFree(newValue)} }    
+            />
+            <FilterSwitch 
+                label='Vegetarian'
+                value={ isVegetarian }
+                onChange={ (newValue) => {setIsVegetarian(newValue)} }    
+            />
         </View>
     );
 };
@@ -52,7 +84,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        width: '80%'
+        width: '80%',
+        marginVertical: 15
     }
 });
 
