2c4631b0431db3e7484cfbf94bc363c733da78f1
rickytorrez <torrez.ricky@ymail.com>
2020-02-25 18:55:34 -0500

    empty list msg

--- react-native/meals/screens/FavoritesScreen.js
+++ react-native/meals/screens/FavoritesScreen.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import MealList from '../components/MealList';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = (props) => {
 
     const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+    if(favMeals.length === 0 || !favMeals){
+        return (
+            <View style={ styles.content }>
+                <DefaultText>No favorite meals found, start adding some!</DefaultText>
+            </View>
+        )
+    };
 
     return <MealList 
                 listData={ favMeals }
@@ -31,6 +40,12 @@ FavoritesScreen.navigationOptions = (navData) => {
     };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default FavoritesScreen;
