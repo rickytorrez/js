cdb285ac5962209c9d41610b98c0e47c803b44eb
rickytorrez <torrez.ricky@ymail.com>
2020-02-27 09:45:58 -0500

    redux intl s/u, models, data

---
+++ react-native/shop/screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+const ProductOverviewScreen = (props) => {
+    return (
+        <FlatList />
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductOverviewScreen;
