d69d60219341cb8a4b17f07b1f94dfa8036fdfe7
rickytorrez <torrez.ricky@ymail.com>
2020-02-28 00:04:01 -0500

    useSelector

--- react-native/shop/screens/shop/ProductOverviewScreen.js
+++ react-native/shop/screens/shop/ProductOverviewScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
+// store import
+import { useSelector } from 'react-redux'; 
+
 const ProductOverviewScreen = (props) => {
+
+    const products = useSelector(state => state.products.availableProducts)
+
     return (
-        <FlatList />
+        <FlatList 
+            data={ products }
+            keyExtractor={ item => item.id }
+            renderItem={ itemData => <Text>{ itemData.item.title }</Text> }
+        />
     );
 };
 
