cb9d17b41f16534251466f0405ce4f592550a4d9
rickytorrez <torrez.ricky@ymail.com>
2020-04-11 16:31:24 -0400

    refactor

--- react-native/shop/components/shop/ProductItem.js
+++ react-native/shop/components/shop/ProductItem.js
@@ -9,6 +9,8 @@ import {
     StyleSheet 
 } from 'react-native';
 
+import Card from '../UI/Card';
+
 const ProductItem = (props) => {
 
     let TouchableComp = TouchableOpacity;
@@ -18,7 +20,7 @@ const ProductItem = (props) => {
     };
 
     return (
-        <View style={ styles.product }>
+        <Card style={ styles.product }>
             <View style={ styles.touchable }>
                 <TouchableComp
                     onPress={ props.onSelect } 
@@ -39,19 +41,12 @@ const ProductItem = (props) => {
                     </View>
                 </TouchableComp>
             </View>
-        </View>
+        </Card>
     );
 };
 
 const styles = StyleSheet.create({
     product: {
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
         height: 300,
         margin: 20,
     },
--- react-native/shop/screens/shop/CartScreen.js
+++ react-native/shop/screens/shop/CartScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 
+import CartItem from '../../components/shop/CartItem';
+import Colors from '../../constants/Colors';
+import Card from '../../components/UI/Card';
+
 import { useSelector, useDispatch } from 'react-redux'
 import * as cartActions from '../../store/actions/cart';
 import * as ordersAction from '../../store/actions/orders';
-import CartItem from '../../components/shop/CartItem';
-import Colors from '../../constants/Colors';
 
 const CartScreen = (props) => {
 
@@ -30,7 +32,7 @@ const CartScreen = (props) => {
 
     return (
         <View style={ styles.screen }>
-            <View style={ styles.summary }>
+            <Card style={ styles.summary }>
                 <Text style={ styles.summaryText }>
                     Total: <Text style={ styles.amount }>${ Math.round(cartTotalAmount.toFixed(2) * 100) / 100}</Text>
                 </Text>
@@ -43,7 +45,7 @@ const CartScreen = (props) => {
                         } 
                     }
                 />
-            </View>
+            </Card>
         
             <FlatList
                 data={ cartItems }
@@ -77,14 +79,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         marginBottom: 20,
-        padding: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white'
+        padding: 10
     },
     summaryText: {
         fontFamily: 'open-sans-bold',
